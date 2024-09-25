e06babed0d7e7813fb6ccbcb4237c55d2814cbfe
Anil Purushothpatnam <anilleo18@gmail.com>
2024-09-25 17:47:11 +0530

    Loacators and Select class Looping

--- my_test/Locators_concept.spec.ts
+++ my_test/Locators_concept.spec.ts
@@ -1,4 +1,4 @@
-import { test, Browser, Page, expect, Locator } from '@playwright/test'
+import { test, Browser, Page, expect, Locator, BrowserContext } from '@playwright/test'
 import { chromium, WebKitBrowser, firefox } from '@playwright/test'
 
 test('Creating Package of locators', async () => {
@@ -6,6 +6,24 @@ test('Creating Package of locators', async () => {
     const browser: Browser = await chromium.launch({ headless: false })
     const page: Page = await browser.newPage();
     await page.goto('https://naveenautomationlabs.com/opencart/index.php?route=account/register');
+
+
+    //Locator get by Role
+
+    await expect(page.getByRole('heading', { name: 'Register Account' })).toBeVisible();
+
+    await expect(page.getByRole('link', { name: 'Transactions' })).toBeVisible();
+
+    await expect(page.getByRole('radio', { name: 'No' })).toBeVisible();
+
+    await page.getByRole('checkbox').click();
+
+    //waitfortieout : Its like thread sleep :
+
+    const continue_button: Locator = page.getByRole('button', { name: 'Continue' });
+    expect(continue_button).toBeVisible();
+    continue_button.click();
+    await page.waitForTimeout(3000);
 
     //create a web element and perform some action  and perform some fill or click action 
 
@@ -40,6 +58,52 @@ test('Creating Package of locators', async () => {
     //We can use getbyTest method : we can design our own testid custom testid attribute
 
     //await page.getByTestId('xpath=//input[@type="submit"]').click();
+})
+
+async function screenshot_mydir(page: Page): Promise<void> {
+    const num: number = Math.random() * 10;
+    await page.screenshot({ path: `test-results/screenshots/homepage${num}.png` })
+}
+
+test.skip('launch in-cognito mode', async () => {
+
+    const Browser_incognio: BrowserContext = await chromium.launchPersistentContext('', { headless: false });
+
+    // It will open array of pages 
+    const pages: Page[] = Browser_incognio.pages();
+
+    //we are pricking 0 th index and continuing page here ...... 
+    const page: Page = pages[0];
+    await page.goto('https://naveenautomationlabs.com/opencart/index.php?route=account/register');
+    await page.waitForTimeout(4000);
+
+
+})
+
+test('Locator chaining strategy', async () => {
+
+
+
+    const broswer: Browser = await chromium.launch();
+    const page: Page = await broswer.newPage();
+    await page.goto("https://www.orangehrm.com/en/30-day-free-trial");
+
+    // const element_form: Locator = page.locator('form#Form_getForm');
+    // const button_trail: Locator = page.getByRole('button', { name: 'Get Your Free Trial' });
+    // await element_form.locator(button_trail).click();
+    // await page.waitForTimeout(3000);
+
+    //orelse dont forget await in locators chaining comcept 
+    // await page.locator('form#Form_getForm').locator('#Form_getForm_subdomain').fill("anil")
+    //await page.locator('form#Form_getForm').getByRole('button', { name: 'Get Your Free Trial' }).click();
+
+    //Reccomended way by playwright team ..This is orginal chaining 
+
+    await page.locator('form#Form_getForm>>#Form_getForm_subdomain').fill("anil");
+    await page.waitForTimeout(3000);
+
+
+
 
 
 
@@ -48,8 +112,3 @@ test('Creating Package of locators', async () => {
 
 
 })
-
-async function screenshot_mydir(page: Page): Promise<void> {
-    const num: number = Math.random() * 10;
-    await page.screenshot({ path: `test-results/screenshots/homepage${num}.png` })
-}
---
+++ my_test/select_concept.spec.ts
@@ -0,0 +1,57 @@
+import { test, Browser, Page, Locator } from '@playwright/test'
+import { chromium } from '@playwright/test'
+
+
+test('Practising with select class', async () => {
+
+    const browser: Browser = await chromium.launch({ headless: false });
+    const page: Page = await browser.newPage();
+    await page.goto('https://www.magupdate.co.uk/reader-enquiry/pbai/212');
+
+    const select_xpath = '//select[@id="Contact_CountryCode"]'
+
+    //want to select with Text
+    await page.selectOption(select_xpath, { label: 'Bahamas' })
+
+
+    // want to select with value 
+    // await page.selectOption(select_xpath, { value: "AR" })
+
+    //want to select with index 
+    //await page.selectOption(select_xpath, { index: 20 })
+
+    await page.waitForTimeout(9000);
+})
+
+
+test('Slelect with looping all the text ', async () => {
+
+    const browser: Browser = await chromium.launch({ headless: false });
+    const page: Page = await browser.newPage();
+    await page.goto('https://www.magupdate.co.uk/reader-enquiry/pbai/212');
+
+    const select_xpath = '//select[@id="Contact_CountryCode"]'
+
+    const lists_options = await page.$$('//select[@id="Contact_CountryCode"]/option');
+
+    console.log(lists_options.length);
+
+
+    for (const element of lists_options) {
+
+        const text_ele = await element.textContent();
+
+        if (text_ele == 'India') {
+
+            await page.selectOption(select_xpath, { label: text_ele });
+            break;
+
+
+        }
+
+    }
+    await page.waitForTimeout(9000);
+
+
+
+})
